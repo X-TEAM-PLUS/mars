b0412eaea609bab3ab94ea1fe7751dc76fc92b3f
yankun <yankun@paypalm.cn>
2018-01-18 12:54:26 +0800

    excle导出下载工具封装

--- mars-common/src/main/java/org/xteam/plus/mars/common/excel/ExcelUtils.java
+++ mars-common/src/main/java/org/xteam/plus/mars/common/excel/ExcelUtils.java
@@ -1,4 +1,133 @@
 package org.xteam.plus.mars.common.excel;
 
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.*;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+/**
+ * Created by yankun on 2017/2/28.
+ */
 public class ExcelUtils {
+
+
+    public static void export(String[] heads, String[] dataIndexs, List data, OutputStream outputStream, String sheetName) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, IOException {
+        XSSFWorkbook workBook = new XSSFWorkbook();
+        XSSFSheet sheet = workBook.createSheet(sheetName);
+        XSSFCellStyle headStyle = getHeadStyle(workBook);
+        XSSFCellStyle bodyStyle = getBodyStyle(workBook);
+        // 构建表头
+        XSSFRow headRow = sheet.createRow(0);
+        XSSFCell cell = null;
+        for (int i = 0; i < heads.length; i++) {
+            cell = headRow.createCell(i);
+            cell.setCellStyle(headStyle);
+            cell.setCellValue(heads[i]);
+        }
+
+        //构建表体数据
+        if(data!=null){
+            for(int rowNum = 0 ; rowNum <data.size(); rowNum++){
+                XSSFRow bodyRow = sheet.createRow(rowNum + 1);
+                for(int columNum = 0 ;columNum<dataIndexs.length;columNum++  ){
+                    cell = bodyRow.createCell(columNum);
+                    cell.setCellStyle(bodyStyle);
+                    cell.setCellValue(BeanUtils.getProperty(data.get(rowNum),dataIndexs[columNum]));
+                }
+            }
+        }
+        workBook.write(outputStream);
+        outputStream.flush();
+    }
+
+
+    /**
+     * 合并单元格后给合并后的单元格加边框
+     *
+     * @param region
+     * @param cs
+     */
+    private void setRegionStyle(CellRangeAddress region, XSSFCellStyle cs, XSSFSheet sheet) {
+        int toprowNum = region.getFirstRow();
+        for (int i = toprowNum; i <= region.getLastRow(); i++) {
+            XSSFRow row = sheet.getRow(i);
+            for (int j = region.getFirstColumn(); j <= region.getLastColumn(); j++) {
+                XSSFCell cell = row.getCell(j);
+                // (short) j);
+                cell.setCellStyle(cs);
+            }
+        }
+    }
+
+    /**
+     * 设置表头的单元格样式
+     *
+     * @return
+     */
+    private static XSSFCellStyle getHeadStyle(XSSFWorkbook wb) {
+        // 创建单元格样式
+        XSSFCellStyle cellStyle = wb.createCellStyle();
+        // 设置单元格的背景颜色为淡蓝色
+        cellStyle.setFillForegroundColor(new XSSFColor(Color.BLUE));
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        // 设置单元格居中对齐
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        // 设置单元格垂直居中对齐
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        // 创建单元格内容显示不下时自动换行
+        cellStyle.setWrapText(true);
+        // 设置单元格字体样式
+        XSSFFont font = wb.createFont();
+        // 设置字体加粗
+        font.setBold(true);
+        font.setFontName("微软雅黑");
+        font.setFontHeight((short) 20);
+        cellStyle.setFont(font);
+        // 设置单元格边框为细线条
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        return cellStyle;
+    }
+
+    /**
+     * 设置表体的单元格样式
+     *
+     * @return
+     */
+    private static XSSFCellStyle getBodyStyle(XSSFWorkbook wb) {
+        // 创建单元格样式
+        XSSFCellStyle cellStyle = wb.createCellStyle();
+        // 设置单元格居中对齐
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        // 设置单元格垂直居中对齐
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        // 创建单元格内容显示不下时自动换行
+        cellStyle.setWrapText(true);
+        // 设置单元格字体样式
+        XSSFFont font = wb.createFont();
+        // 设置字体加粗
+        font.setBold(true);
+        font.setFontName("微软雅黑");
+        font.setFontHeight((short) 20);
+        cellStyle.setFont(font);
+        // 设置单元格边框为细线条
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        return cellStyle;
+    }
+
 }
