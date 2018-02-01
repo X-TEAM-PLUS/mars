5f2a741f8b97419f05bbdb0f764e841b3844d104
yankun <yankun@paypalm.cn>
2018-02-01 19:28:28 +0800

    添加访问域配置

--- mars-service/src/main/java/org/xteam/plus/mars/config/AppConfiguration.java
+++ mars-service/src/main/java/org/xteam/plus/mars/config/AppConfiguration.java
@@ -3,6 +3,9 @@ package org.xteam.plus.mars.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.xteam.plus.mars.common.DateTimeConverter;
 
 import java.util.Date;
@@ -12,5 +15,18 @@ public class AppConfiguration {
     @Bean
     public Converter<String, Date> addNewConvert() {
         return new DateTimeConverter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
