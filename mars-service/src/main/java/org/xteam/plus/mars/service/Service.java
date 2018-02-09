f4909376a1e1775039d9ae8d262da4e3156b7058
yankun <yankun@paypalm.cn>
2018-02-09 11:27:46 +0800

    跨域名配置

--- mars-service/src/main/java/org/xteam/plus/mars/service/Service.java
+++ mars-service/src/main/java/org/xteam/plus/mars/service/Service.java
@@ -4,7 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,7 +23,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //启注解事务管理
 @EnableTransactionManagement
 @EnableDiscoveryClient
+@Configuration
 public class Service {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
     public static void main(String[] args) {
         // 程序启动入口
         SpringApplication.run(Service.class, args);
