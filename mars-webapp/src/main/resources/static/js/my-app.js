6d64755e7e78354c606713b9f62fe3584e92fad1
yankun <yankun@paypalm.cn>
2018-03-27 11:31:19 +0800

    分享健康卡

--- mars-webapp/src/main/resources/static/js/my-app.js
+++ mars-webapp/src/main/resources/static/js/my-app.js
@@ -755,18 +755,18 @@ function shardWeixin(cardNo) {
                 nonceStr: bizContent.nonceStr, // 必填，生成签名的随机串
                 signature: bizContent.signature,// 必填，签名，见附录1
                 jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ',
-                    'onMenuShareQZone','translateVoice']
+                    'onMenuShareQZone']
                 // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
             wx.ready(function() {
-                app.dialog.alert('http://' + document.domain + '/index.html?' + cardNo);
-
+                app.dialog.alert(bizContent.shardLink);
                 wx.onMenuShareAppMessage({
                     title: '健康卡购买', // 分享标题
                     desc: '用户给您分享了他的健康卡', // 分享描述
                     link: bizContent.shardLink, // 分享链接
+                    path:'/index.html?cardNo'+cardNo,
                     imgUrl: sellUserInfo.wxHeadPortrait, // 分享图标
-                    type: '', // 分享类型,music、video或link，不填默认为link
+                    type: 'link', // 分享类型,music、video或link，不填默认为link
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
                         // 用户确认分享后执行的回调函数
