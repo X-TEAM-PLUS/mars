943f477bf79fa6aadc82889e725fd3103f69e30e
songxinlei <songxinlei@paypalm.cn>
2018-04-27 00:26:53 +0800

    1)导入功能开发

--- mars-admin/src/main/resources/templates/datacenter/importPolicy.ftl
+++ mars-admin/src/main/resources/templates/datacenter/importPolicy.ftl
@@ -53,7 +53,7 @@
                     <div class="table-toolbar">
                         <div class="row">
                             <div class="col-md-12 pull-right">
-                                <form id="ordersTableForm" onsubmit="init();return false;">
+                                <form id="ordersTableForm" name="ordersTableForm" class="form-horizontal"  onsubmit="init();return false;">
                                     <div class="input-group">
                                         <div class="input-icon col-md-2">
                                             <select id="cardType" name="status" class="form-control selectpicker"
@@ -85,7 +85,7 @@
                                         <span class="btn btn-success fileinput-button">
                                             <i class="fa fa-plus"></i>
                                             <span>保险导入</span>
-                                            <input name="files[]" multiple="" type="file"> </span>
+                                            <input name="uploadFile" id="uploadFile" type="file" onchange="uploadImport()"> </span>
                                         </div>
 
                                     </div>
@@ -130,7 +130,7 @@
 <script src="${request.contextPath}/static/assets/global/plugins/jquery-file-upload/js/jquery.fileupload-audio.js" type="text/javascript"></script>
 <script src="${request.contextPath}/static/assets/global/plugins/jquery-file-upload/js/jquery.fileupload-video.js" type="text/javascript"></script>
 <script src="${request.contextPath}/static/assets/global/plugins/jquery-file-upload/js/jquery.fileupload-validate.js" type="text/javascript"></script>
-<script src="${request.contextPath}/static/assets/global/plugins/jquery-file-upload/js/jquery.fileupload-ui.js"/>
+<#--<script src="${request.contextPath}/static/assets/global/plugins/jquery-file-upload/js/jquery.fileupload-ui.js"/>-->
 <script>
     function init() {
         $("#ordersTable").pagingGrid(
@@ -163,37 +163,34 @@
         );
     }
 
+    function uploadImport(){
+        alert("upload");
+        var oMyForm = new FormData();
+        oMyForm.append("uploadFile", $('#uploadFile')[0].files[0])
+        $('body').append('<div id="layer"></div>');
+        $.ajax({
+            url : "/services/mars/policyinfo/uploadImport",
+            type : "POST",
+            async:false,
+            contentType: false,    //这个一定要写
+            processData: false, //这个也一定要写，不然会报错
+            data : oMyForm,
+            success : function(data) {
+                alert(data.message);
+                $('#layer').remove();
+            },
+            error : function(data) {
+                $( '#serverResponse').html(data.status + " : " + data.statusText + " : " + data.responseText);
+                $('#layer').remove()
+            }
+        });
+    }
+
     //初始化
     init();
 
 </script>
 
-<script id="template-upload" type="text/x-tmpl"> {% for (var i=0, file; file=o.files[i]; i++) { %}
-                        <tr class="template-upload fade">
-                            <td>
-                                <span class="preview"></span>
-                            </td>
-                            <td>
-                                <p class="name">{%=file.name%}</p>
-                                <strong class="error text-danger label label-danger"></strong>
-                            </td>
-                            <td>
-                                <p class="size">Processing...</p>
-                                <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                                    <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-                                </div>
-                            </td>
-                            <td> {% if (!i && !o.options.autoUpload) { %}
-                                <button class="btn blue start" disabled>
-                                    <i class="fa fa-upload"></i>
-                                    <span>Start</span>
-                                </button> {% } %} {% if (!i) { %}
-                                <button class="btn red cancel">
-                                    <i class="fa fa-ban"></i>
-                                    <span>Cancel</span>
-                                </button> {% } %} </td>
-                        </tr> {% } %}
-</script>
 </body>
 </html>
 
--- mars-service/src/main/java/org/xteam/plus/mars/service/provider/PolicyInfoServiceProvider.java
+++ mars-service/src/main/java/org/xteam/plus/mars/service/provider/PolicyInfoServiceProvider.java
@@ -1,9 +1,11 @@
 package org.xteam.plus.mars.service.provider;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.xteam.plus.mars.common.JsonResult;
-import org.xteam.plus.mars.common.excel.CellFormater;
 import org.xteam.plus.mars.common.excel.ExcelUtils;
 import org.xteam.plus.mars.domain.PolicyInfo;
 import org.xteam.plus.mars.manager.PolicyInfoManager;
@@ -13,9 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -28,7 +28,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/mars/policyinfo")
 public class PolicyInfoServiceProvider extends AbstractServiceProvider {
-    private static final String CHARSET= "UTF-8";
+    private static final String CHARSET = "UTF-8";
     @Resource
     private PolicyInfoManager policyinfoManager;
 
@@ -64,21 +64,21 @@ public class PolicyInfoServiceProvider extends AbstractServiceProvider {
     @RequestMapping(value = "/export")
     public void export(HttpServletResponse response, PolicyInfo policyinfo) {
         OutputStream out = null;
-        try{
+        try {
             //查询
             policyinfo.setLimit(Integer.MAX_VALUE);
             List<PolicyInfo> data = policyinfoManager.query(policyinfo);
             //下载文件名
-            String fileName ="下保单(全部)数据.xlsx";
+            String fileName = "下保单(全部)数据.xlsx";
             String sheetName = "全部";
-            if(policyinfo.getStartDate()!=null && policyinfo.getEndDate()!=null) {
+            if (policyinfo.getStartDate() != null && policyinfo.getEndDate() != null) {
                 sheetName = new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getStartDate()) + "-" + new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getEndDate());
                 fileName = "下保单(" + sheetName + ")数据.xlsx";
-            }else if (policyinfo.getStartDate()!=null ){
-                sheetName ="从" + new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getStartDate())+ "起";
+            } else if (policyinfo.getStartDate() != null) {
+                sheetName = "从" + new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getStartDate()) + "起";
                 fileName = "下保单(" + sheetName + ")数据.xlsx";
-            }else if(policyinfo.getEndDate()!=null){
-                sheetName = "截止到"+new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getStartDate())+ "";
+            } else if (policyinfo.getEndDate() != null) {
+                sheetName = "截止到" + new SimpleDateFormat("yyyy年MM月dd日").format(policyinfo.getStartDate()) + "";
                 fileName = "下保单(" + sheetName + ")数据.xlsx";
             }
             // 设置响应参数
@@ -89,46 +89,80 @@ public class PolicyInfoServiceProvider extends AbstractServiceProvider {
             //表头
             String[] header = {
                     "会员卡号"
-                    ,"姓名"
-                    ,"年龄"
-                    ,"出生日期"
-                    ,"身份证号"
-                    ,"手机号"
-                    ,"联系地址"
-                    ,"激活日期"
-                    ,"保费"
-                    ,"会员卡有效期"
-                    ,"续期缴费次数"
+                    , "姓名"
+                    , "年龄"
+                    , "出生日期"
+                    , "身份证号"
+                    , "手机号"
+                    , "联系地址"
+                    , "激活日期"
+                    , "保费"
+                    , "会员卡有效期"
+                    , "续期缴费次数"
             };
             //获取dataIndex
             String[] dataIndexs = {
                     "cardNo"
-                    ,"realName"
-                    ,"age"
-                    ,"birthDate"
-                    ,"idNumber"
-                    ,"mobileNo"
-                    ,"linkAddress"
-                    ,"cardActivateTime"
-                    ,"premium"
-                    ,"cardLifeTime"
-                    ,"payTimes"
+                    , "realName"
+                    , "age"
+                    , "birthDate"
+                    , "idNumber"
+                    , "mobileNo"
+                    , "linkAddress"
+                    , "cardActivateTime"
+                    , "premium"
+                    , "cardLifeTime"
+                    , "payTimes"
             };
             //导出
-            ExcelUtils.export(header,dataIndexs,data,out,sheetName);
-        }catch (Exception e){
+            ExcelUtils.export(header, dataIndexs, data, out, sheetName);
+        } catch (Exception e) {
             logError("导出数据时异常", e);
-        }finally {
-            if(out != null){
+        } finally {
+            if (out != null) {
                 try {
                     out.flush();
                     out.close();
                 } catch (IOException e) {
-                    logError("close BufferedOutputStream error:" +e);
+                    logError("close BufferedOutputStream error:" + e);
                 }
 
             }
         }
     }
 
+    @RequestMapping(value = "/uploadImport")
+    @ResponseBody
+    public JsonResult uploadImport(@RequestParam(required = false) MultipartFile uploadFile) {
+        JsonResult jsonResult = new JsonResult();
+        try {
+            if (uploadFile == null) {
+                throw new Exception("导出文件为空!");
+            }
+            //获取dataIndex
+            String[] dataIndexs = {
+                    "cardNo"
+                    , "realName"
+                    , "age"
+                    , "birthDate"
+                    , "idNumber"
+                    , "mobileNo"
+                    , "linkAddress"
+                    , "cardActivateTime"
+                    , "premium"
+                    , "cardLifeTime"
+                    , "payTimes"
+            };
+            List<PolicyInfo> policyInfo = ExcelUtils.load(dataIndexs, PolicyInfo.class, uploadFile.getInputStream());
+            jsonResult.setMessage("导入成功  总导入"+policyInfo.size());
+            jsonResult.setSuccess(true);
+        } catch (Exception e) {
+            logError("导入异常!", e);
+            jsonResult.setMessage("导入异常");
+            jsonResult.setSuccess(false);
+
+        } finally {
+            return jsonResult;
+        }
+    }
 }
